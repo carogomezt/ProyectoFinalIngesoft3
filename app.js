eb265bdd3925845e88bf258e5ba4acee6afa1d2b
Sebastian Duque <sebas_tian_95@hotmail.com>
2016-05-10 20:40:35 -0500

    Se arreglo la version de express

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
-
-/**
- * Module dependencies.
- */
-
-var express = require('express')
-  , http = require('http')
-  , path = require('path')
-  , methodOverride = require('method-override')
-  , session = require('express-session');
+const express = require('express'),
+      path = require('path'),
+      favicon = require('static-favicon'),
+      logger = require('morgan'),
+      cookieParser = require('cookie-parser'),
+      bodyParser = require('body-parser'),
+      methodOverride = require('method-override'),
+      session = require('express-session');
 
 var app = express();
 
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-//app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
+
+app.use(favicon());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(methodOverride('_method'));
 app.use(session({
   secret: "sgasnfklashdfasmd",
@@ -31,9 +32,10 @@ app.use(function(req, res, next){
   next();
 });
 
-require('./routes/index')(app);
-require('./routes/login')(app);
+require('./routes/index')(app, '/');
+//require('./routes/menu')(app, '/menu');
 
+/// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
@@ -49,8 +51,7 @@ if (app.get('env') === 'development') {
         res.status(err.status || 500);
         res.render('error', {
             message: err.message,
-            error: err,
-            errors: []
+            error: err
         });
     });
 }
@@ -61,8 +62,7 @@ app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
-        error: {},
-        errors: []
+        error: {}
     });
 });
 
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,32 @@
+const express = require('express');
+var router = express.Router();
 
-/*
- * GET home page.
- */
+var users = [{"mesa": 1, "pwd": "123"},
+             {"mesa": 2, "pwd": "123"},
+            ];
 
-module.exports = function(app){
-    app.get('/', function(req, res) {
-       res.render('index'); 
-    });
+module.exports = function(app, mountPoint) {
+  app.get('/', function(req, res){
+    res.render('index');
+  });
+
+  app.post('/login', function(req, res) {
+    var mesa = req.body.mesa;
+    var pass = req.body.pwd;
+
+    for(var i in users) {
+      if(users[i].mesa == mesa && users[i].pwd == pass) {
+        req.session.users = users[i];
+        res.render('menu');
+      }
+    }
+    res.redirect('/login');
+  });
+
+  app.delete('/logout', function(req, res) {
+    delete req.session.users;
+    res.redirect('/');
+  });
+
+  app.use(mountPoint, router);
 }
